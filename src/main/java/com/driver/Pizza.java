2a5742d70213520e74fdab716adf6fd33b20ad11
Avishek <savishek@gmail.com>
2023-02-25 23:05:43 +0530

    Pizza-Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,28 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
     private String bill;
+    private int toppings;
+    private Boolean isVeg;
+    private boolean extraCheeseAdded;
+    private boolean extraToppingsAdder;
+    private boolean is_BillCreated;
+    private boolean bag;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg)
+        {
+            this.price=300;
+            this.toppings=70;
+        }
+        else
+        {
+            this.price=400;
+            this.toppings=120;
+        }
+        this.bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -16,19 +32,46 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!extraCheeseAdded)
+        {
+            this.price+=80;
+            extraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingsAdder)
+        {
+            this.price+=this.toppings;
+            extraToppingsAdder=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!bag)
+        {
+            this.price+=20;
+            bag=true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        if(!is_BillCreated) {
+            if (extraCheeseAdded) {
+                this.bill += "Extra Cheese Added: 80" +"\n";
+            }
+            if (extraToppingsAdder) {
+                this.bill += "Extra Toppings Added: " + this.toppings +"\n";
+            }
+            if (bag) {
+                this.bill += "Paperbag Added: 20" +"\n";
+            }
+            this.bill += "Total Price: " + this.price;
+            this.is_BillCreated=true;
+            return this.bill;
+        }
+        return " ";
     }
 }
